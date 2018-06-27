862b9167e3ff52239545bffd1fd98b24fa76755c
Jonas Kubilius <qbilius@gmail.com>
2018-06-27 16:23:59 -0400

    memo -> memo_db (outdated)

--- memo.py
+++ memo.py
@@ -1,144 +1,129 @@
 #!/usr/bin/env python
-import sys, os, time, shutil, argparse, subprocess, datetime, contextlib, pickle, inspect, socket
-from collections import OrderedDict
-# import requests
-import numpy as np
-import pandas
-import seaborn as sns
-# from watchdog.observers import Observer
-# from watchdog.events import FileSystemEventHandler
+import sys, os, shutil, argparse, subprocess, socket, datetime, json
 
-ROOT = '/braintree/home/qbilius/dropbox/memo'
-SCRATCH = '/braintree/home/qbilius/computed'
-DATA_DIR = os.path.join(ROOT, 'store')
-DBPATH = os.path.join(ROOT, 'index.csv')
-pandas.set_option('display.max_colwidth', -1)
+DATA_DIR = '/braintree/data2/active/users/qbilius/memo'
 
 
-class PandasDB(object):
-
-    def __init__(self):
-        self.write = 'MEMO_DIR' in os.environ
-        if self.write:
-            self.path = os.path.join(os.environ['MEMO_DIR'])
-
-    def __getitem__(self, key):
-        return PandasColl(self.path, key)
-
-    @contextlib.contextmanager
-    def open_file(self, filename):
-        f = open(os.path.join(self.path, filename), 'wb')
-        yield f
-        f.close()
-
-    def savefig(self, filename, *args, **kwargs):
-        sns.plt.savefig(os.path.join(self.path, filename), *args, **kwargs)
-
-    def save_to_scratch(self, obj, filename):
-        frame = inspect.getouterframes(inspect.currentframe())[1]
-        caller = os.path.relpath(frame.filename, '/braintree/home/qbilius/dropbox')
-        path = os.path.join(SCRATCH, os.path.splitext(caller)[0], filename)
-        pickle.dump(obj, open(path, 'wb'))
+def append_timestamp():
+    rec = json.load(open(os.environ['MEMO_DIR'] + 'meta.json', 'r'))
+    rec['end_time'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    json.dump(rec, open(os.environ['MEMO_DIR'] + 'meta.json', 'w'), indent=4)
 
 
-class PandasColl(object):
-
-    def __init__(self, path, name):
-        self.path = os.path.join(path, name + '.csv')
-
-    def append(self, new):
-        try:
-            df = pandas.read_csv(self.path, index_col=0, na_values='NaN', keep_default_na=False)
-        except:
-            df = new
-        else:
-            df = pandas.concat([df, new], ignore_index=True)
-        df.to_csv(self.path, encoding='utf-8')
-
-# def write_to_db(rec):
-#     db = pandas.read_csv(DBPATH, index_col=0, na_values='')
-#     db = db.append(rec, ignore_index=True)
-#     db.to_csv(DBPATH, encoding='utf-8')
-
-
-def setup_dest():
-    db = pandas.read_csv(DBPATH, index_col=0, na_values='NaN', keep_default_na=False)
-    os.environ['MEMO_IDX'] = f"{len(db):04d}"
-
-    dest = os.path.join(DATA_DIR, os.environ['MEMO_IDX'])
-    if not os.path.isdir(dest):
-        os.makedirs(dest)
-        os.environ['MEMO_DIR'] = dest + os.path.sep
-    else:
-        raise ValueError('{} already exists'.format(dest))
-    print(f'memo id: {os.path.basename(dest)}, version: {version}')
-    return dest
-
-
-def store(args=None, dest=None):
-    start = time.time()
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-    if args is None:
-        args = sys.argv[1:]
-
+def main(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('executable')
     parser.add_argument('script')
     parser.add_argument('-t', '--tag', default='')
     parser.add_argument('-d', '--description', default='')
-    args, other = parser.parse_known_args(args)
+    parser.add_argument('--slurm', action='store_true', default=False)
+    parser.add_argument('--switch_dir', action='store_true', default=True)
+    args, script_args = parser.parse_known_args(args)
 
-    call_args = [args.executable, args.script] + other
-    command = ' '.join(call_args)
+    if args.slurm:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-t', '--time', default='2-00:00:00')
+        parser.add_argument('-n', '--ntasks', default=1, type=int)
+        parser.add_argument('-c', '--cpus_per_task', default=5, type=int)
+        parser.add_argument('--gres', default='gpu:GEFORCEGTX1080TI:1')  # titan-x, GEFORCEGTX1080TI
+        parser.add_argument('--mem', default='40G')
+        parser.add_argument('--qos', action='store_true', default=False)
+        parser.add_argument('--job_name', default=None)
 
-    db = pandas.read_csv(DBPATH, index_col=0, na_values='NaN', keep_default_na=False)
+        slurm_kwargs, script_args = parser.parse_known_args(script_args)
+        if slurm_kwargs.qos:
+            slurm_kwargs.qos = 'dicarlo'
+        else:
+            delattr(slurm_kwargs, 'qos')
 
-    if command in db.command.values:
-        version = db[db.command == command].version.max() + 1
+        if slurm_kwargs.job_name is None:
+            delattr(slurm_kwargs, 'job_name')
+
+    # memo_idx = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(4))
+    memo_idx = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    os.environ['MEMO_DIR'] = os.path.join(DATA_DIR, memo_idx) + os.sep
+
+    if not os.path.isdir(os.environ['MEMO_DIR']):
+        os.makedirs(os.environ['MEMO_DIR'])
+        print('memo id:', memo_idx)
     else:
-        version = 1
+        raise ValueError('{} already exists'.format(os.environ['MEMO_DIR']))
 
-    os.environ['MEMO_CMD_VERSION'] = str(version)
-    # db.to_csv('/braintree/home/qbilius/dropbox/memo/index.csv', encoding='utf-8')
-    # sys.exit()
-    # dest = os.path.join(DATA_DIR, '{:04d}_{}'.format(len(db), timestamp))
+    shutil.copy2(sys.argv[2], os.environ['MEMO_DIR'])
 
-    if dest is not None:
-        dest = setup_dest()
-    shutil.copy2(os.path.abspath(args.script), dest)
+    command = ' '.join(script_args)
+    if args.slurm and os.path.basename(args.executable) == 'python':
+        command = f'{sys.executable} {args.script} {command}'
+    else:
+        command = f'{args.executable} {args.script} {command}'
 
-    rec = OrderedDict([('timestamp', timestamp),
-                       ('command', command),
-                       ('host', socket.gethostname()),
-                       ('working dir', os.path.abspath(os.getcwd())),
-                       ('duration', np.nan),
-                       ('version', version),
-                       ('tag', args.tag),
-                       ('description', args.description),
-                       ('outcome', ''),
-                       ('show', True)
-                       ])
-    db = db.append(rec, ignore_index=True)
-    idx = db.index[-1]
+    if args.slurm:
+        with open(os.environ['MEMO_DIR'] + 'run.sh', 'w') as f:
+            f.write('#!/bin/sh\n')
+            for k,v in slurm_kwargs.__dict__.items():
+                key = k.replace('_', '-')
+                key = f'-{key}' if len(key) == 1 else f'--{key}'
+                skip = False
+                if key == 'qos':
+                    if v:
+                        v = 'dicarlo'
+                    else:
+                        skip = True
+                elif key == 'jobname' and v is None:
+                    skip = True
 
-    db.to_csv(DBPATH, encoding='utf-8')
+                if not skip:
+                    f.write(f'#SBATCH {key}={v}\n')
 
-    # db = pandas.read_csv(DBPATH, index_col=0, na_values='')
+            f.write('\n')
+            f.write(f"export MEMO_DIR={os.environ['MEMO_DIR']}\n")
+            if args.executable == 'python':
+                f.write(f'singularity exec --bind /braintree:/braintree '
+                        f'--bind /home:/home --bind /om:/om --nv '
+                        f'docker://nvidia/cuda:9.0-cudnn7-runtime-centos7 '
+                        f'"{command}"\n')
+            else:
+                f.write(f'"{command}"\n')
+
+            f.write('memo append_timestamp\n')
+
+        call_args = ['ssh', 'openmind7.mit.edu', 'sbatch', os.environ['MEMO_DIR'] + 'run.sh']
+
+    else:
+        with open(os.environ['MEMO_DIR'] + 'run.sh', 'w') as f:
+            f.write(f'{command}\n')
+            f.write('memo append_timestamp\n')
+
+        call_args = ['sh', os.environ['MEMO_DIR'] + 'run.sh']
+
+    rec = {'start_time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'args': script_args,
+            'host': socket.gethostname(),
+            'working dir': os.path.abspath(os.getcwd()),
+            'tag': args.tag,
+            'description': args.description,
+            'outcome': '',
+            'slurm': args.slurm,
+            'switch_dir': args.switch_dir,
+            'show': True}
+
+    json.dump(rec, open(os.environ['MEMO_DIR'] + 'meta.json', 'w'), indent=4)
+
     # requests.post('http://localhost:5000/wait-for-changes',
     #               data={'data': db.loc[db.index[-1:]].drop('show', 1).to_html()})
-
-    p = subprocess.Popen(call_args)
-    try:
-        p.wait()
-    except KeyboardInterrupt:
-        p.terminate()
-
-    dur = time.time() - start
-    db = pandas.read_csv(DBPATH, index_col=0, na_values='')
-    db.loc[idx, 'duration'] = int(dur)
-    db.to_csv(DBPATH, encoding='utf-8')
+    subprocess.Popen(['nohup'] + call_args,
+                     stdin=open('/dev/null', 'w'),
+                     stdout=open(os.environ['MEMO_DIR'] + 'log.out', 'a'),
+                     stderr=open(os.environ['MEMO_DIR'] + 'log.out', 'a'))
 
 
 if __name__ == '__main__':
-    store()
+    if len(sys.argv) == 1:
+        print('nothing to do')
+    else:
+        if sys.argv[1] == 'append_timestamp':
+            append_timestamp()
+        elif sys.argv[1] == 'smemo':
+            main(sys.argv + ['--slurm'])
+        else:
+            main()
